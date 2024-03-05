90ad9e609e86f25241ebbf158b90ed91c36e4272
Edson Ifarraguirre Moreno <10083346@facin.portoalegre.pucrsnet.br>
2024-03-05 10:46:45 -0300

    Commit inicial

---
+++ Aula02_29Fev/main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(){
+  int valor1, valor2;
+  printf("Digite dois valores: ");
+  scanf("%d %d", &valor1, &valor2);
+  printf("O valor 1 é %d\n",valor1);
+  printf("O valor 2 é %d\n",valor2);
+}
